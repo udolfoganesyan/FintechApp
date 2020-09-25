2cd86f786ea88777abda86858443d84b5c069743
Rudolf Oganesyan <rude97@yandex.ru>
2020-09-26 01:03:46 +0300

    navigation bar

--- FintechApp/ViewControllers/ConversationsList/ConversationsListViewController.swift
+++ FintechApp/ViewControllers/ConversationsList/ConversationsListViewController.swift
@@ -24,7 +24,34 @@ class ConversationsListViewController: UIViewController {
         
         view.backgroundColor = .white
 
+        setupNavigationBar()
         setupTableView()
+    }
+    
+    private func setupNavigationBar() {
+        title = "Tinkoff Chat"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let settingsButton = UIBarButtonItem(image: #imageLiteral(resourceName: "settings"), style: .plain, target: nil, action: nil)
+        settingsButton.tintColor = UIColor(red: 0.329, green: 0.329, blue: 0.345, alpha: 0.65)
+        navigationItem.leftBarButtonItem = settingsButton
+        
+        let avatarView = AvatarImageView(style: .circle)
+        let avatarViewContainer = UIView()
+        avatarView.install(on: avatarViewContainer)
+        avatarView.setupWith(firstName: "Rudolf", lastName: "Oganesyan", color: #colorLiteral(red: 0.8941176471, green: 0.9098039216, blue: 0.168627451, alpha: 1))
+        let profileButton = UIBarButtonItem(customView: avatarViewContainer)
+        profileButton.customView?.translatesAutoresizingMaskIntoConstraints = false
+        profileButton.customView?.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        profileButton.customView?.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleProfileTap))
+        avatarViewContainer.addGestureRecognizer(tapGesture)
+        navigationItem.setRightBarButton(profileButton, animated: true)
+    }
+    
+    @objc private func handleProfileTap() {
+        let profileViewController = ProfileViewController()
+        present(profileViewController, animated: true)
     }
     
     private func setupTableView() {
