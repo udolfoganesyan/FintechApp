6a86d7101c9501e8baf2dba6544e28599912ca95
Rudolf Oganesyan <rude97@yandex.ru>
2020-12-01 14:22:42 +0300

    faster ui tests

--- FintechApp/AppDelegate.swift
+++ FintechApp/AppDelegate.swift
@@ -37,5 +37,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     private func configureAppForUITesting() {
         UIView.setAnimationsEnabled(false)
+        self.window?.layer.speed = 100
     }
 }
